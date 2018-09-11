e14f7e7dfbbd63214b32718dcd111a06588f7771
KJH2013 <panda9417@gmail.com>
2018-09-11 16:17:49 +0900

    hi
    hi

---
+++ src/day02/Ex_02.java
@@ -0,0 +1,19 @@
+package day02;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("JJJJJJJJJJ     AAAAAAA    VV         VV     AAAAAAA"); 
+		System.out.println("JJJJJJJJJJ    AAAAAAAAA    V         V     AAAAAAAAA"); 
+		System.out.println("       JJJ   AAA     AAA    VV      VV    AAA     AAA"); 
+		System.out.println("       JJJ  AAA       AAA    V      V    AAA       AAA"); 
+		System.out.println("       JJJ AAAAAAAAAAAAAAA    V    V   AAAAAAAAAAAAAAAA"); 
+		System.out.println("J     JJJ  AAAAAAAAAAAAAAAA    V  V   AAAAAAAAAAAAAAAAAA"); 
+		System.out.println("JJ   JJJ  AAA            AAA    V V  AAA              AAA"); 
+		System.out.println(" JJJJJ   AAA              AAA    V  AAA                AAA"); 
+		System.out.println("  JJJ   AAA                AAA   V AAA                  AAA"); 
+
+	}
+
+}
